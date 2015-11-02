c4e8f4dca98b1f6386cd583e90cc91e09ff9956c
Stephane <boluge@gmail.com>
2015-11-02 16:40:48 +0100

    v0.9.3

--- wplandingpage-premium.php
+++ wplandingpage-premium.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Landing Page PREMIUM
 Plugin URI: http://www.b5prod.com
 Description: Aditionally Templates for Plugin WP Landing Page
-Version: 0.9.2
+Version: 0.9.3
 Author: B5 Productions
 Author URI: http://www.b5prod.com
 License: GPL2
@@ -27,6 +27,33 @@ along with WP Landing Page. If not, see https://www.gnu.org/licenses/gpl-2.0.htm
 
 if( ! class_exists( 'WPLP_Updater' ) ){
 	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+/* Auto install WP LP and actived */
+require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+add_action( 'tgmpa_register', 'my_plugin_register_required_plugins' );
+
+function my_plugin_register_required_plugins() {
+ 	$plugins = array(
+ 		array(
+ 			'name'      		=> 'WP Landing Page',
+			'slug'      		=> 'wp-landing-page',
+ 			'required' 	 		=> true,
+			'force_activation'  => true,
+ 		)
+ 	);
+
+	$config = array(
+ 		'id'           => 'wplpp',                 // Unique ID for hashing notices for multiple instances of TGMPA.                     // Default absolute path to bundled plugins.
+ 		'menu'         => 'wplpp-install-plugins', // Menu slug.
+ 		'parent_slug'  => 'plugins.php',            // Parent menu slug.
+ 		'capability'   => 'parent_menu_slug',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+ 		'has_notices'  => true,                    // Show admin notices or not.
+ 		'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+ 		'dismiss_msg'  => 'To use WP Landing Page Premium you need install WP Landing Page',                      // If 'dismissable' is false, this message will be output at top of nag.
+ 		'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+	);
+	tgmpa( $plugins, $config );
 }
 
 $updater = new WPLP_Updater( __FILE__ );
